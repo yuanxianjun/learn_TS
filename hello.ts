5ca37ab1ea2cce0d8fae3e238b9c65f5ff68e036
原县军 <1872391190@qq.com>
2020-06-15 10:24:55 +0800

    first commit

---
+++ hello.ts
@@ -0,0 +1,11 @@
+function sayHello(person: string) {
+    return 'Hello,' + person
+}
+let user = [1, 2, 3, 4]
+console.log(sayHello(user))
+
+// 五种数据类型：布尔 数值 字符串 null undefined 
+let isBool: Boolean = false;
+let creatBool: Boolean = false;
+
+let createBynewBoll: Boolean = new Boolean();
